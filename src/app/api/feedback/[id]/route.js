26e8e428663990b7b995340f7096d5ad8bf4e470
mokim <mamokim2005@gmail.com>
2026-01-28 23:32:43 +0600

    patch functionality done

--- src/app/api/feedback/[id]/route.js
+++ src/app/api/feedback/[id]/route.js
@@ -1,8 +1,6 @@
 import { connect } from "@/app/lib/dbConnect";
 import { ObjectId } from "mongodb";
 
-
-
 const feedbackCollection = await connect("feedbacks");
 
 export async function GET(request, { params }) {
@@ -17,12 +15,10 @@ export async function GET(request, { params }) {
 
   const query = { _id: new ObjectId(id) };
 
-  const result = await feedbackCollection.findOne(query)
+  const result = await feedbackCollection.findOne(query);
 
   return Response.json(result);
 }
-
-
 
 export async function DELETE(request, { params }) {
   const { id } = await params;
@@ -36,7 +32,35 @@ export async function DELETE(request, { params }) {
 
   const query = { _id: new ObjectId(id) };
 
-  const result = await feedbackCollection.deleteOne(query)
+  const result = await feedbackCollection.deleteOne(query);
+
+  return Response.json(result);
+}
+
+export async function PATCH(request, { params }) {
+  const { id } = await params;
+  const { message } = await request.json();
+
+  if (id.length !== 24) {
+    return Response.json({
+      status: 400,
+      message: "send correct _id",
+    });
+  }
+
+  if (!message || typeof message !== "string") {
+    return Response.json({ message: "Please send a message" }, { status: 400 });
+  }
+
+  const query = { _id: new ObjectId(id) };
+
+  const newData = {
+    $set: {
+      message,
+    },
+  };
+
+  const result = await feedbackCollection.updateOne(query, newData);
 
   return Response.json(result);
 }
