3b57938bdedcfb08322f83a747e82f465ff76912
mokim <mamokim2005@gmail.com>
2026-01-29 21:11:09 +0600

    some editing

--- src/app/feedback/add/page.jsx
+++ src/app/feedback/add/page.jsx
@@ -1,9 +1,9 @@
 import FeedbackForm from "@/components/forms/FeedbackForm";
 
-
 const AddFeedback = () => {
   return (
     <div>
+      <h2 className="text-2xl py-3 text-center">Add Feedback</h2>
       <FeedbackForm></FeedbackForm>
     </div>
   );
